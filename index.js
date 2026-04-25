54a862679e5f898d7f4207f4c3bbbf8ca0b3298c
Preetham M.N <preetham@Preethams-MacBook-Air.local>
2026-04-25 14:14:31 +0530

    initial changes

---
+++ index.js
@@ -0,0 +1,12 @@
+// index.js
+const express = require("express");
+const app = express();
+
+app.get("/health", (req, res) => {
+  res.json({ status: "UP", time: new Date() });
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log("Server started on port " + PORT);
+});
