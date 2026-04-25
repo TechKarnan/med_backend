4a02c68e2edd312c1cc4021eaae8397e1d376b5a
Preetham M.N <preetham@Preethams-MacBook-Air.local>
2026-04-25 14:19:46 +0530

    added log

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/health", (req, res) => {
+    console.log("Health check endpoint called");
   res.json({ status: "UP", time: new Date() });
 });
 
